cbc743196aec9aa677888c96ef5c0d08df209acb
11meghana0 <11meghana0@gmail.com>
2026-06-12 11:35:09 -0500

    Add ExpensesController with GET and POST endpoints

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Expense_Tracker.Models;
+
+namespace Expense_Tracker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExpensesController : ControllerBase
+    {
+        // Temporary in-memory list to store expenses
+        private static List<Expense> expenses = new List<Expense>();
+
+        // GET: api/expenses
+        [HttpGet]
+        public ActionResult<List<Expense>> GetAll()
+        {
+            return Ok(expenses);
+        }
+
+        // POST: api/expenses
+        [HttpPost]
+        public ActionResult<Expense> Create(Expense expense)
+        {
+            expense.Id = expenses.Count + 1;
+            expense.Date = DateTime.Now;
+            expenses.Add(expense);
+            return CreatedAtAction(nameof(GetAll), expense);
+        }
+    }
+}
